379f86242aebbd571b737b4eb8042e9ed82c76de
zkhzkhz <1742864433@qq.com>
2020-04-15 16:05:48 +0800

    dfe

--- gintest/main_test.go
+++ gintest/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/smartystreets/goconvey/convey"
+	_ "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -8,6 +10,15 @@ import (
 )
 
 func TestPingRoute(t *testing.T) {
+	convey.Convey("Given a HTTP request for /invalid/123", t, func() {
+		//req := httptest.NewRequest("GET", "/ping", nil)
+		resp := httptest.NewRecorder()
+		convey.Convey("When the request is handled by the Router", func() {
+			convey.Convey("Then the response should be a 404", func() {
+				convey.So(resp.Code, convey.ShouldEqual, 404)
+			})
+		})
+	})
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.12
 
 require (
 	github.com/astaxie/beego v1.12.0 // indirect
-	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
 	golang.org/x/net v0.0.0-20191007182048-72f939374954 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
